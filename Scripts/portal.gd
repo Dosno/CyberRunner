93ff3ce7d7df043ae27d776719ce140bfb678ec0
ceejyc41 <ceejyc41@gmail.com>
2026-08-26 09:43:05 +0800

    fixed portal not working

--- Scripts/portal.gd
+++ Scripts/portal.gd
@@ -4,15 +4,17 @@ extends Area2D
 @export var destination_city: String = ""
 
 func _on_body_entered(body: Node2D) -> void:
+	print("PORTAL DETECTED: ", body.name)
+	print("Is player group? ", body.is_in_group("player"))
+
 	if body.is_in_group("player"):
-		# Save this portal's destination as the new checkpoint
+		print("TELEPORTING PLAYER!")
+
 		GameManager.checkpoint_position = destination.global_position
 		GameManager.has_checkpoint = true
 
-		# Teleport player
 		body.global_position = destination.global_position
 
-		# Show city sign
 		if destination_city != "":
-			print("CitySignManager = ", CitySignManager)
+			print("Showing city: ", destination_city)
 			CitySignManager.show_city(destination_city)
